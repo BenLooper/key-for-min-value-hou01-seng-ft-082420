24170a1d40b69e091f2763a66a8ae779467e0abc
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-21 17:00:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  name_array = []
+  age_array = []
+  unless name_hash == {}
+    name_hash.each do |name, age|
+      name_array.append(name)
+      age_array.append(age)
+    
 end
