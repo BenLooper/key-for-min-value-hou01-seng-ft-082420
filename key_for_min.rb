97cba4423c3a42e94dc92bd749edfb100173851b
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-21 17:22:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   name_string = ""
   min_age = 1000000 
-  unless name_hash == {} do  
+  unless name_hash == {} then  
     name_hash.each do |name, age|
       if age < min_age 
         min_age = age 
